02d85d72df1f506779b6b82256b5f73128a1105d
Fábio Assunção <fabio23gt@gmail.com>
2018-11-30 23:44:08 -0300

    Links simbólicos para apps, storage e backups na basta hoje

--- setup-vps.sh
+++ setup-vps.sh
@@ -7,7 +7,7 @@ function setup_log() {
 
 # necessário ser root
 if [ "$(id -u)" != "0" ]; then
-   echo "Desculpe! Este script deve ser executado como root." 1>&2
+   setup_log "Desculpe! Este script deve ser executado como root." 1>&2
    exit 1
 fi
 
@@ -15,7 +15,7 @@ fi
 setup_log "Este script executará as configurações iniciais neste servidor."
 read -r -p "Digite 'S' para continuar ou qualquer tecla para cancelar: " GO
 if [ "$GO" != "S" ]; then
-    echo "Aborting." 1>&2
+    setup_log "Aborting." 1>&2
     exit 1
 fi
 
@@ -26,7 +26,7 @@ apt-get dist-upgrade
 apt-get autoremove
 dpkg-reconfigure tzdata
 
-echo "\n"
+echo -e "\n"
 
 # define senha root
 setup_log "Definindo a senha do root..."
@@ -50,7 +50,7 @@ if [ ! -e /root/.ssh/authorized_keys ]; then
 	touch /root/.ssh/authorized_keys
 fi
 
-echo "\n"
+echo -e "\n"
 
 # adiciona bitbucket.org, gitlab.com, github.com
 setup_log "Adicionando bitbucket.org aos hosts confiáveis..."
@@ -62,7 +62,7 @@ ssh-keyscan gitlab.com >> /root/.ssh/known_hosts
 setup_log "Adicionando github.com aos hosts confiáveis..."
 ssh-keyscan github.com >> /root/.ssh/known_hosts
 
-echo "\n"
+echo -e "\n"
 
 # pedir nome de usuário do novo usuário padrão
 read -r -p "Insira um username para o usuário que fará deploy de aplicações (Ex.: deployer):" DEPLOYER_USERNAME
@@ -71,7 +71,7 @@ if [ -z "$DEPLOYER_USERNAME" ]; then
     exit 1
 fi
 
-echo "\n"
+echo -e "\n"
 
 # pedir nome de "vendor" que será utilizado como prefixo nas pastas de apps, storage e backups. Ex.: nome de um organização como google ou codions
 read -r -p "Insira um nome de pasta padrão onde ficarão os apps, storage e backups (Ex.: suaempresa): " VENDOR_NAME
@@ -80,14 +80,14 @@ if [ -z "$VENDOR_NAME" ]; then
     exit 1
 fi
 
-echo "\n"
+echo -e "\n"
 
 # adiciona usuário padrão
 setup_log "Criando usuário padrão..."
 useradd -s /bin/bash -d /home/$DEPLOYER_USERNAME -m -U $DEPLOYER_USERNAME
 passwd $DEPLOYER_USERNAME
 
-echo "\n"
+echo -e "\n"
 
 # copia SSH authorized_keys
 setup_log "Copiando a chave pública SSH para diretório home do novo usuário padrão..."
@@ -97,31 +97,31 @@ fi
 cp -r /root/.ssh/* /home/$DEPLOYER_USERNAME/.ssh/
 chown -R $DEPLOYER_USERNAME.$DEPLOYER_USERNAME /home/$DEPLOYER_USERNAME/.ssh
 
-echo "\n"
+echo -e "\n"
 
 # adiciona usuário padrão aos sudoers
 setup_log "Adicionando $DEPLOYER_USERNAME aos sudoers com todos os privilégios..."
 echo "$DEPLOYER_USERNAME ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$DEPLOYER_USERNAME
 chmod 0440 /etc/sudoers.d/$DEPLOYER_USERNAME
 
-echo "\n"
+echo -e "\n"
 
 # instala git, zip, unzip
 setup_log "Instalando programas essenciais (git, zip, unzip, curl)..."
 apt-get install -y git zip unzip curl wget
 
-echo "\n"
+echo -e "\n"
 
 setup_log "Instalando docker..."
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 
-echo "\n"
+echo -e "\n"
 
 setup_log "Instalando docker-compose..."
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-echo "\n"
+echo -e "\n"
 
 setup_log "Adicionando usuário $DEPLOYER_USERNAME no grupo www-data..."
 usermod -aG www-data $DEPLOYER_USERNAME
@@ -141,7 +141,16 @@ mkdir -p /var/$VENDOR_NAME/backups
 setup_log "Mudando proprietário do diretório de trabalho de root para $DEPLOYER_USERNAME..."
 chown -R $DEPLOYER_USERNAME.$DEPLOYER_USERNAME /var/$VENDOR_NAME
 
-echo "\n"
+echo -e "\n"
+
+setup_log "Criando link simbólico para a pasta de aplicações..."
+ln -s /var/$VENDOR_NAME/apps /home/$DEPLOYER_USERNAME/apps
+
+setup_log "Criando link simbólico para a pasta storage..."
+ln -s /var/$VENDOR_NAME/storage /home/$DEPLOYER_USERNAME/storage
+
+setup_log "Criando link simbólico para a pasta de backups..."
+ln -s /var/$VENDOR_NAME/backups /home/$DEPLOYER_USERNAME/storage
 
 # cleanup
 setup_log "Limpando..."
